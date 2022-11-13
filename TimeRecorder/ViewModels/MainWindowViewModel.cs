5546fb6410a88efe805887bc33c705ad41804a67
Akn <sibata486@gmail.com>
2022-11-14 00:28:51 +0900

    init

---
+++ TimeRecorder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace TimeRecorder.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public MainWindowViewModel()
+        {
+        }
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
