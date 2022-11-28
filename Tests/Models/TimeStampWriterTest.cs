3de9647d4bcf4d3a8b7921aae7c07fde73e11e4c
Akn <sibata486@gmail.com>
2022-11-28 15:37:28 +0900

    add / ユニットテストプロジェクトを追加

---
+++ Tests/Models/TimeStampWriterTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using TimeRecorder.Models;
+
+namespace Tests.Models;
+
+[TestFixture]
+public class TimeStampWriterTest
+{
+    [Test]
+    public void テキスト出力テスト1()
+    {
+        var writer = new TimeStampWriter();
+    }
+}
