e322f743f49580b50ce17d786173b487c3a7f47e
Akn <sibata486@gmail.com>
2023-02-10 17:18:39 +0900

    add / Todo.BaseId プロパティを追加

--- TimeRecorder/Models/TimeStamp.cs
+++ TimeRecorder/Models/TimeStamp.cs
@@ -13,6 +13,17 @@ namespace TimeRecorder.Models
         [Required]
         public int GroupId { get; set; }
 
+        /// <summary>
+        /// 特定の TimeStamp を修正した祭、その TimeStamp の情報を変更するのではなく、別の新しい TimeStamp を生成する。
+        /// 新しく生成したものには編集元の TimeStamp の Id をこの BaseId に記録する。
+        /// </summary>
+        /// <value>
+        /// 生成元の TimeStamp の Id を記録。
+        /// 生成元が存在しない新規 TimeStamp の場合は 0 とする。
+        /// </value>
+        [Required]
+        public int BaseId { get; set; }
+
         [Required]
         public DateTime DateTime { get; set; } = DateTime.Now;
 
