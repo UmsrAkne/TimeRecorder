83534e1b5c84da69c84e0ab642908ec8ae1983df
Akn <sibata486@gmail.com>
2023-04-06 00:03:10 +0900

    add feat / タイムスタンプ出力時に全体の合計時間を添付する機能を追加

--- Tests/Models/TimeStampWriterTest.cs
+++ Tests/Models/TimeStampWriterTest.cs
@@ -25,6 +25,7 @@ public class TimeStampWriterTest
     public void テキスト出力テスト複数行()
     {
         var writer = new TimeStampWriter();
+        writer.AttachTotalTime = false;
 
         List<TimeStamp> timeStamps = new List<TimeStamp>()
         {
@@ -36,6 +37,27 @@ public class TimeStampWriterTest
         var expect = "2022/11/28 01:01:01 TimeStamp1\r\n"
                      + "2022/11/28 01:01:02 TimeStamp2\r\n"
                      + "2022/11/28 01:01:03 TimeStamp3";
+
+        Assert.AreEqual(expect, writer.GetTimeStampString(timeStamps));
+    }
+
+    [Test]
+    public void テキスト出力テスト複数行合計時間付き()
+    {
+        var writer = new TimeStampWriter();
+        writer.AttachTotalTime = true;
+
+        List<TimeStamp> timeStamps = new List<TimeStamp>()
+        {
+            new (){ DateTime = new DateTime(2022, 11, 28, 1, 1, 1), Comment = "TimeStamp1" },
+            new (){ DateTime = new DateTime(2022, 11, 28, 1, 1, 2), Comment = "TimeStamp2" },
+            new (){ DateTime = new DateTime(2022, 11, 28, 1, 1, 3), Comment = "TimeStamp3" },
+        };
+
+        var expect = "2022/11/28 01:01:01 TimeStamp1\r\n"
+                     + "2022/11/28 01:01:02 TimeStamp2\r\n"
+                     + "2022/11/28 01:01:03 TimeStamp3\r\n"
+                     + "Total : 00:00:02";
 
         Assert.AreEqual(expect, writer.GetTimeStampString(timeStamps));
     }
