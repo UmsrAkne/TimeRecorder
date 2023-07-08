b66d866560c2cb2e620958996d2e7c609493792f
Akn <sibata486@gmail.com>
2023-07-08 19:22:44 +0900

    add file / TimeStamp を格納するクラスに実装するインターフェースを追加

---
+++ TimeRecorder/Models/Converters/IDataSource.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace TimeRecorder.Models.Converters;
+
+public interface IDataSource
+{
+    int Count { get; }
+
+    void Add(TimeStamp timeStamp);
+
+    void Add(TimeStampGroup group);
+
+    /// <summary>
+    ///     新しいグループを追加し、そのグループの id をパラメーターの GroupId に割り当てます
+    /// </summary>
+    /// <param name="ts">新しいグループに追加するタイムスタンプ</param>
+    void AddNewGroup(TimeStamp ts);
+
+    List<TimeStamp> GetTimeStamps(TimeStampGroup targetGroup);
+
+    TimeStamp GetTimeStamp(int id);
+
+    List<TimeStampGroup> GetGroups();
+
+    TimeStampGroup GetLatestGroup();
+}
